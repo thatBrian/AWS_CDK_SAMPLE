927a01f9842f8d1b41b41059124e29f6c233e983
Noor Sheikh <noorsheikh.me@gmail.com>
2022-12-19 22:58:16 -0500

    Initial commit

---
+++ test/brian-aws-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BrianAwsCdk from '../lib/brian-aws-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/brian-aws-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BrianAwsCdk.BrianAwsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
